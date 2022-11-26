0d5d0a2183ba5ae681fd12c4674862152920deee
Piaowei <msptonmoy@gmail.com>
2022-11-27 04:37:39 +0600

    admin menue

--- ticket_booking_client/src/components/defaultLayouts/DefaultLayout.js
+++ ticket_booking_client/src/components/defaultLayouts/DefaultLayout.js
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../resources/layout.css';
 
 const DefaultLayout = ({ children }) => {
+	const navigate = useNavigate();
+	const [colapsed, setColapsed] = useState(false);
+	const adminMenu = [
+		{
+			name: "Home",
+			path: "/",
+			icon: "ri-home-line",
+		},
+		{
+			name: "Buses",
+			path: "/admin/buses",
+			icon: "ri-bus-line",
+		},
+		{
+			name: "Users",
+			path: "/admin/users",
+			icon: "ri-user-line",
+		},
+		{
+			name: "Bookings",
+			path: "/admin/bookings",
+			icon: "ri-file-list-line",
+		},
+		{
+			name: "Logout",
+			path: "/logout",
+			icon: "ri-logout-box-line",
+		},
+	];
+	// const menuToBeRendered = user?.isAdmin ? adminMenu : userMenu;
+	const menuToBeRendered = adminMenu;
+	let activeRoute = window.location.pathname;
 	return (
-		<div>
-			<h1>Admin Header</h1>
-			{children}
+		<div className='layout-parent' >
+			<div className='sidebar'>
+				<div className='d-flex flex-column gap-2'>
+					{menuToBeRendered.map((item, index) => {
+						return <div className={`${activeRoute === item.path && "active-menu-item"
+							} menu-item`}>
+							<i className={item.icon}></i>
+							{!colapsed && (
+								<span onClick={() => {
+									if (item.path == '/logout') {
+										navigate('/login');
+										localStorage.removeItem('token');
+									}
+									else {
+										navigate(item.path);
+									}
+								}} >{item.name}</span>)}
+						</div>;
+					})}
+				</div>
+			</div>
+			<div className='body'>
+				<div className='header' >
+					{!colapsed ? (<i
+						class="ri-close-line"
+						onClick={() => setColapsed(!colapsed)}
+					></i>
+					) : (<i class="ri-menu-2-fill" onClick={() => setColapsed(!colapsed)}></i>)
+					}
+				</div>
+				<div className='content' >{children}</div>
+			</div>
 		</div>
 	);
 };
